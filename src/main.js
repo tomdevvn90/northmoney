d05f2dbbdb8466948b9edbf32662c4b3f86829bd
Mike.tropi <mike.beplus@gmail.com>
2021-05-17 09:02:08 +0700

    First commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+/**
+ * Script 
+ */
+
+import './scss/main.scss'
+
+;( ( w, $ ) => {
+  'use strict'
+
+} )( window, jQuery )
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+/**
+ * Webpack config 
+ */
+
+const path = require( 'path' )
+const webpack = require( 'webpack' )
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isDev = process.argv.some( arg => arg.includes( 'serve' ) )
+
+module.exports = {
+  mode: isDev ? 'development' : 'production',
+  entry: {
+    app: './src/main.js'
+  },
+  output: {
+    path: path.resolve( __dirname, 'public' ),
+    filename: 'js/[name].bundle.js',
+    publicPath: '/public/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin( {
+      filename: 'css/[name].css',
+    } ),
+    new webpack.ProvidePlugin( {
+      $: "jquery",
+      jQuery: "jquery"
+    } )
+  ],
+  devServer: {
+    open: true,
+    contentBase: path.join( __dirname ),
+    compress: true,
+    port: 9000,
+  },
+}
